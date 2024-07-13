0e257e613356e5cc107bc1ba33c217f6b8f19285
Dambo389 <150391167+Dambo389@users.noreply.github.com>
2024-07-13 13:08:42 +0200

    Add files via upload

---
+++ dambopc.bat
@@ -0,0 +1,19 @@
+@Echo off
+@echo Hi, choose the right team that you need
+:m1
+Echo 1 - disable task manager
+Echo 2 - enable task manager
+Echo 3 - shutdown pc
+Echo 4 - control your phone by wire
+
+Set /p choice="d: "
+if not defined choice goto m1'
+if "%choice%"=="1" (cd C:\Dambopc
+start disabletaskmanager.exe)
+if "%choice%"=="2" (cd C:\Dambopc
+start enabletaskmanager.exe)
+if "%choice%"=="3" (cd C:\Dambopc
+start shutdownpc.exe)
+if "%choice%"=="4" (start phone.bat)
+goto m1
+pause
