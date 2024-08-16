6f787a76822dae3044fe73d361bcadfb5450910d
Dambo389 <150391167+Dambo389@users.noreply.github.com>
2024-08-16 23:18:13 +0200

    phone.bat

--- phone.bat
+++ phone.bat
@@ -2,9 +2,11 @@
 echo in order for this to work, you need to do this https://github.com/Dambo389/phone/tree/main when you have done this, connect the phone by wire
 :m1
 Echo 1 - reboot phone
+Echo 1 - remote access
 Set /p choice="d: "
 if not defined choice goto m1'
 if "%choice%"=="1" (cd adb
 adb reboot)
+if "%choice%"=="2" (cd adb)
 goto m1
 pause
